bc8ad0374d4a2f06dc7f4ac36db3a922fb102d27
Himel-HSTU <mdhabiburrahmanhimel27@gmail.com>
2025-12-20 23:14:13 +0600

    Updae

--- ReadWriteBasics/Program.cs
+++ ReadWriteBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.WriteLine("This is a Write Line Method,means cover the whole line,next start from next line");
 //snippets is just cw for Console.WriteLine()
 Console.Write("This is Just  Write not Write Line Method,Next start after this finish;");
-Console.Write("As you can see,this is another write method");
+Console.Write("As you can see,this is another write method\n");
+Console.ReadLine();
