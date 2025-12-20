7c4569a67be12c6cdb4ac49413c060a0576d0f1e
Himel-HSTU <mdhabiburrahmanhimel27@gmail.com>
2025-12-21 00:28:00 +0600

    Update

---
+++ GreetingApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿//This is a Basic Greeting Application in C# by following Tim Corey C# MasterClass No.17-18
+
+// Welcoming User
+Console.WriteLine("Welcome to my Greeting Application!!");
+Console.WriteLine("------------------------------------");
+Console.WriteLine();
+Console.WriteLine();
+//Taking User Input n Outputting it Back
+Console.Write("Please Enter Your First Name: ");
+string firstName = Console.ReadLine();
+Console.WriteLine("Here's Your First Name: "+ firstName);
+Console.WriteLine();
+Console.Write("Please Enter Your Last Name: ");
+string lastName=Console.ReadLine();
+Console.WriteLine("Here's Your Last Name: "+lastName);
+Console.WriteLine();
+Console.WriteLine("Here's Your Full Name: "+firstName+" "+lastName);
+Console.WriteLine();
+Console.WriteLine("Thank You for Using My Greeting Application!!");
