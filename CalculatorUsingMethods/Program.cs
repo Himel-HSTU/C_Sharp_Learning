d14f921bdb900f54bd328db1fad8bac8297f162e
Himel-HSTU <mdhabiburrahmanhimel27@gmail.com>
2026-02-26 01:52:09 +0600

    26/02/2026

---
+++ CalculatorUsingMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿// In this program we will create a simple calculator that can perform basic arithmetic operations such as addition, subtraction, multiplication, and division. We will define methods for each operation and then call these methods from the Main method.
+using CalculatorUsingMethods;
+
+Console.Write("Hello,What is Your Name: ");
+string name = Console.ReadLine();
+if (name == "")
+{
+    Console.Write("Please Enter a Correct Name: ");
+    name = Console.ReadLine();
+}
+Console.WriteLine($"Welcome {name},To Our Basic Calculator (Using Methods)");
+bool wantMore = true;
+while (wantMore)
+{
+    Console.Write("What Type of Operation You Wanna Perform (eg. +,-,*,/): ");
+    string operation = Console.ReadLine();
+    Console.WriteLine($"OK!!! So, You Wanna Perform {operation}, We Need Two Numbers!");
+    Console.Write("Enter The First  Number: ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter The Second Number: ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"So The Numbers are: {num1} and {num2} ");
+    // Now we will call the appropriate method based on the user's choice of operation
+    if (operation == "+")
+        operations.add(num1, num2);
+    
+    else if (operation == "-")
+        operations.sub(num1, num2);
+    
+    else if (operation == "*")
+        operations.multi(num1, num2);
+    
+    else if (operation == "/")
+        operations.div(num1, num2);
+
+    Console.Write("Wanna Do Another Operation? (Y/N) : ");
+
+    string response= Console.ReadLine();
+
+    if (response == "Y" || response == "y")
+        wantMore = true;
+    else if (response == "N" || response == "n")
+        wantMore = false;
+}
---
+++ CalculatorUsingMethods/operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorUsingMethods;
+
+    public static class operations
+    {
+    public static void add(double n1,double n2)
+    {
+        Console.WriteLine($"The Result of {n1}+{n2} is {n1+n2}");
+    }
+    public static void sub(double n1, double n2)
+    {
+        Console.WriteLine($"The Result of {n1}-{n2} is {n1 - n2}");
+    }
+    public static void multi (double n1, double n2)
+    {
+        Console.WriteLine($"The Results of {n1}*{n2} is {n1 * n2}");
+    }
+    public static void div(double n1,double n2)  {
+        if (n2 == 0)
+            Console.WriteLine("Denominator can't be 0.... Dustuu:(");
+
+        else Console.WriteLine($"The Result of {n1}/{n2} is {n1 / n2}");
+    }
+}
+
